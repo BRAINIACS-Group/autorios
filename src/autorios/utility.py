4325a8227ae95aab5597b71c302acf8b995ca12c
Philipp Scherm <philipp.scherm@fau.de>
2026-06-02 16:40:56 +0200

    add click_before option to write (float) to input; set edit function to circumvent send keys in write input

--- src/autorios/utility.py
+++ src/autorios/utility.py
@@ -42,7 +42,7 @@ def escape_keyboard_string(keyboard_string:str)->str:
         keyboard_string = keyboard_string.replace(escape_char,f"{{{escape_char}}}")
     return keyboard_string
 
-def write_to_input(element,input_str:str,press_tab:bool=False,escape_special_chars:bool=False):
+def write_to_input(element,input_str:str,press_tab:bool=False,escape_special_chars:bool=False,click_before:bool=True):
     '''Type string to input field
     Args:
         element: element object from pywinauto
@@ -51,14 +51,15 @@ def write_to_input(element,input_str:str,press_tab:bool=False,escape_special_cha
     Raises:
     '''
     
-    element.click_input()
+    if click_before:
+        element.click_input()
     if escape_special_chars:
         input_str = escape_keyboard_string(input_str)
     type_str = '^a'+input_str
     if press_tab: type_str += "{TAB}"
     send_keys(type_str)
 
-def write_float_to_input(element,input_value:float,press_tab:bool=False, draw_outline:bool=True):
+def write_float_to_input(element,input_value:float,press_tab:bool=False, draw_outline:bool=True, click_before:bool=True):
     '''Type string to input field
     Args:
         element: element object from pywinauto
@@ -70,7 +71,33 @@ def write_float_to_input(element,input_value:float,press_tab:bool=False, draw_ou
     if DECIMAL_SEPARATOR != '.':
         input_str = input_str.replace('.',DECIMAL_SEPARATOR)
     if draw_outline: element.draw_outline()
-    write_to_input(element,input_str,press_tab=press_tab)
+    write_to_input(element,input_str,press_tab=press_tab, click_before=click_before)
+
+def set_edit_str_to_input(element,input_str:str,press_tab:bool=False, draw_outline:bool=True):
+    '''Set text of Edit element to input_str
+    Args:
+        element: element object from pywinauto
+        input_str: string to be written to the edit box
+    Returns:
+    Raises:
+    '''
+    if draw_outline: element.draw_outline()
+    element.set_edit_text(input_str)
+    if press_tab:
+        send_keys("{TAB}")
+
+def set_edit_float_to_input(element,input_value:float,press_tab:bool=False, draw_outline:bool=True):
+    '''Set text of Edit element to input_value
+    Args:
+        element: element object from pywinauto
+        input_value: float value to be written to the edit box
+    Returns:
+    Raises:
+    '''
+    input_str = str(input_value)
+    if DECIMAL_SEPARATOR != '.':
+        input_str = input_str.replace('.',DECIMAL_SEPARATOR)
+    set_edit_str_to_input(element,input_str,press_tab=press_tab, draw_outline=draw_outline)
 
 def open_filexplorer(filepath:Path)->None:
     subprocess.Popen(fr'explorer /select,"{str(filepath.resolve())}"')
